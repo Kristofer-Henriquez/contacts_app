92a501fdc47a80fe073886db55cd1b398199a36d
Kristofer <kristoferhenriquez@gmail.com>
2020-06-29 20:48:10 -0500

    created model and added 3 contacts to app, created route and a controller rendering a json file for first contact on app

--- app/controllers/api/contacts_pages_controller.rb
+++ app/controllers/api/contacts_pages_controller.rb
@@ -1,2 +1,6 @@
 class Api::ContactsPagesController < ApplicationController
+  def run
+    @first_contact = Contact.first
+    render "first_contact.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/contactsurl" => "contacts_pages#run"
+  end
 end
